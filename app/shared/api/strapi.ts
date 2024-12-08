fb730b227a0c499d4ad0e30eb7b19917234e5067
unknown <animalbtw@gmail.com>
2024-12-08 19:59:41 +0300

    test player

--- app/shared/api/strapi.ts
+++ app/shared/api/strapi.ts
@@ -2,7 +2,7 @@ import strapiInstance from "./axios"
 
 export const getAudio = async () => {
     try {
-        return await strapiInstance.get('/api/audio?populate=*')
+        return await strapiInstance.get('api/audio?populate=*')
     } catch(e) {
         console.log(`err: >> ${e}`)
         return null
--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_STRAPI: 'https://gitfcms.up.railway.app/api'
-  }
+  async redirects() {
+    return [
+      {
+        source: '/api',
+        destination: 'https://gitfcms.up.railway.app/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
