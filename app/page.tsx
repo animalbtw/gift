7d51073fa9e7b070ff83a1fc973c1176c75c5347
unknown <animalbtw@gmail.com>
2024-12-08 22:45:31 +0300

    test player

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
 import AudioPlayer from 'react-h5-audio-player';
 import { getAudio, getCounter, updateCounter } from "./shared/api/strapi";
+import { Snowfall } from "./widgets/snowfall";
+import bg from '../public/bg.png'
 import style from './assets/styles/page.module.css'
 import anim from './assets/styles/animations.module.css'
 
@@ -17,10 +20,12 @@ export default function Home() {
     getCounter().then(res => setStreamings(res?.data.data.streaming))
   }, [])
 
-  
-
   return (
     <div className={classNames('container', anim.slide)}>
+      <div className={style.background}>
+        <Image src={bg} alt="bg" layout='fill' />
+        <Snowfall />
+      </div>
       <AudioPlayer
         className={style.player_container}
         src={`https://gitfcms.up.railway.app${audio}`}
---
+++ app/widgets/snowfall/snowfall.tsx
@@ -0,0 +1,9 @@
+import style from './snowfall.module.css'
+
+export const Snowfall = () => {
+	return (
+		<div className={style.snow}>
+			<div className={style.snowflower}></div>
+		</div>
+	)
+}
