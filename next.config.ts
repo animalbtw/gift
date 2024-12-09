8c5a4c316a97c05e6df8576be3ae33b640ce2159
unknown <animalbtw@gmail.com>
2024-12-09 19:37:05 +0300

    change tree position & animation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['three'],
+
   async redirects() {
     return [
       {
